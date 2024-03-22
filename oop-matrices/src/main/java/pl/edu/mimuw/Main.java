dc4578efb6332ac16dd65de80ffef4e8f33c4029
mpkitlo <mpkitlo@gmail.com>
2024-03-22 20:09:57 +0100

    .

--- oop-matrices/src/main/java/pl/edu/mimuw/Main.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.mimuw;
-
-public class Main {
-
-  public static void main(String[] args) {
-    // Tu trzeba wpisać kod testujący toString dla poszczególnych macierzy i wyników operacji
-  }
-}
